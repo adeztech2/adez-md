821fb2c678f0a69d0d5544343f2407ef4d0d20ee
Arnold Adez <adeztech2@gmail.com>
2026-09-20 10:51:22 +0300

    Refactor ping command to use object syntax

--- commands/ping.js
+++ commands/ping.js
@@ -1,22 +1,26 @@
-module.exports.name = "ping";
-module.exports.category = "General";
-module.exports.adminOnly = false;
-module.exports.ownerOnly = false;
+module.exports = {
+    name: 'ping',
+    aliases: ['p'],
+    description: 'Check if the bot is alive and see response speed',
+    ownerOnly: false,
 
-module.exports.execute = async function (sock, msg, ctx) {
-  const start = Date.now();
+    async execute(sock, msg, ctx) {
 
-  const sent = await sock.sendMessage(
-    ctx.from,
-    { text: '🏓 Pinging...' },
-    { quoted: msg }
-  );
+        const start = Date.now();
 
-  const latency = Date.now() - start;
+        const sent = await sock.sendMessage(
+            ctx.from,
+            { text: '🏓 Pinging...' },
+            { quoted: msg }
+        );
 
-  await sock.sendMessage(
-    ctx.from,
-    { text: `🏓 Pong!\n⏱️ ${latency}ms` },
-    { quoted: sent }
-  );
+        const ms = Date.now() - start;
+
+        await sock.sendMessage(
+            ctx.from,
+            { text: `🏓 Pong! ${ms}ms` },
+            { quoted: msg }
+        );
+
+    }
 };
